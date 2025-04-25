84c2e69ede4dc847997d4ac704fe04dfabe1af5f
raychzhang <raychzhang@tencent.com>
2025-04-25 22:44:32 +0800

    update lc151 原地修改字符串

--- LeetCode/lc151反转字符串中的单词/main.cc
+++ LeetCode/lc151反转字符串中的单词/main.cc
@@ -11,15 +11,15 @@ void reverseString(std::string& s, size_t start_idx, size_t end_idx) {
   }
 }
 
-string reverseWords(string s) {
-  size_t st_idx = 0, ed_idx = 0;
+string reverseWords2(string s) {
+  size_t st_idx = 0, ed_idx = 0;  
   string results;
   while (st_idx < s.size()) {
     if (s[st_idx] == ' ') {
       st_idx++;
     } else {
       ed_idx = st_idx;
-      while (s[ed_idx] != ' ' && ed_idx < s.size()) {
+      while (ed_idx < s.size() && s[ed_idx]) {
         ed_idx++;
       }
       // reverseString(s, st_idx, ed_idx - 1);
@@ -32,6 +32,32 @@ string reverseWords(string s) {
   // reverseString(results, 0, results.size() - 1);
   std::reverse(results.begin(), results.end());
   return results;
+}
+
+// 原地修改s，但lc上执行用时反而比方法1慢？
+string reverseWords(string s) {
+  size_t st_idx = 0, ed_idx = 0;  // 原字符串中单词的起始和结尾
+  size_t new_idx = 0;  // 去掉多余空格后的字符串中单词的起始和结尾
+  while (st_idx < s.size()) {
+    if (s[st_idx] == ' ') {
+      st_idx++;
+    } else {
+      ed_idx = st_idx;
+      size_t new_ed = new_idx;
+      while (ed_idx < s.size() && s[ed_idx] != ' ') {
+        s[new_ed ++] = s[ed_idx ++];
+      }
+      std::reverse(s.begin() + new_idx, s.begin() + new_ed); // 反转单词
+      s[new_ed ++] = ' ';  // 单车末尾加一个空格
+
+      st_idx = ed_idx;
+      new_idx = new_ed;
+    }
+  }
+  if (new_idx) new_idx--;
+  s.erase(s.begin() + new_idx, s.end());
+  std::reverse(s.begin(), s.end());
+  return s;
 }
 
 int main() {
